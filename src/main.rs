5ac258ec36dbfb144fa1ccfc766d04699e220ebb
Richard <richardkenyon1991@hotmail.co.uk>
2024-01-03 16:22:46 +0000

    create Line enum with variants

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::env::args;
+use std::error::Error;
+
+struct NameData {
+    name: String,
+}
+struct NameAndNumberData {
+    name: String,
+    number: i32,
+}
+
+enum Line {
+    Name(NameData),
+    NameAndNumber(NameAndNumberData),
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let file_name = args().nth(1).ok_or("Please provide a file name")?;
+
+    Ok(())
 }
